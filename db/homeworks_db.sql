0c9653665d2063310fafdcbc1ce0577f7d85b28b
JhonnSantiagoBernalJuradoCampus <jhonbernalcampus@gmail.com>
2023-07-19 09:23:15 -0500

    feat: :sparkles: Database
    Creacion de las tablas de la base de datos

---
+++ db/homeworks_db.sql
@@ -0,0 +1,38 @@
+CREATE DATABASE homeworks_db;
+USE homeworks_db;
+CREATE TABLE user(
+    usu_id INT(11) PRIMARY KEY AUTO_INCREMENT,
+    usu_nombre VARCHAR(200) NOT NULL,
+    usu_email VARCHAR(100) NOT NULL,
+    usu_telefono VARCHAR(50) NOT NULL,
+    usu_apodo VARCHAR(50) NOT NULL,
+    usu_created_at TIMESTAMP NOT NULL,
+    usu_updated_at TIMESTAMP NOT NULL
+);
+CREATE TABLE estado(
+    estado_id INT(11) PRIMARY KEY AUTO_INCREMENT,
+    estado_nombre VARCHAR(100) NOT NULL,
+    estado_created_at TIMESTAMP NOT NULL,
+    estado_updated_at TIMESTAMP NOT NULL
+);
+CREATE TABLE tipo(
+    tipo_id INT(11) PRIMARY KEY AUTO_INCREMENT,
+    tipo_nombre VARCHAR(200) NOT NULL,
+    tipo_created_at TIMESTAMP NOT NULL,
+    tipo_updated_at TIMESTAMP NOT NULL
+);
+CREATE TABLE tarea(
+    tarea_id INT(11) PRIMARY KEY AUTO_INCREMENT,
+    tarea_titulo VARCHAR(100) NOT NULL,
+    tarea_descripcion VARCHAR(255) NOT NULL,
+    tarea_fecha DATE NOT NULL,
+    tarea_recordatorio DATETIME NOT NULL,
+    tarea_created_at TIMESTAMP NOT NULL,
+    tarea_updated_at TIMESTAMP NOT NULL,
+    id_user INT,
+    id_estado INT,
+    id_tipo INT,
+    FOREIGN KEY (id_user) REFERENCES user(usu_id),
+    FOREIGN KEY (id_estado) REFERENCES estado(estado_id),
+    FOREIGN KEY (id_tipo) REFERENCES tipo(tipo_id)
+);
