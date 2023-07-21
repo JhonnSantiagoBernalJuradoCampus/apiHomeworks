bb4b26363fb3a24e84283340e23e0afae8501882
JhonnSantiagoBernalJuradoCampus <jhonbernalcampus@gmail.com>
2023-07-21 08:20:01 -0500

    feat: :sparkles: Validacion de le data

--- middleware/proxyTipo.js
+++ middleware/proxyTipo.js
@@ -6,6 +6,10 @@ const proxyTipo = (req, res, next) => {
     try {
         let data = plainToClass(dtoTipo, req.body, { excludeExtraneousValues: true });
         req.body = data;
+        let values = Object.values(req.body);
+        values.forEach((val,id)=>{
+            if(val ==undefined) throw {status: 401, message: "Los parametros son obligatorios"}
+        })
         next();
     } catch (error) {
         res.status(error.status).send(error.message);
--- middleware/proxyUser.js
+++ middleware/proxyUser.js
@@ -6,6 +6,10 @@ const proxyUser = (req, res, next) => {
     try {
         let data = plainToClass(dtoUser, req.body, { excludeExtraneousValues: true })
         req.body = data;
+        let values = Object.values(req.body);
+        values.forEach((val,id)=>{
+            if(val ==undefined) throw {status: 401, message: "Los parametros son obligatorios"}
+        })
         next();
     } catch (error) {
         res.status(error.status).send(error.message);
