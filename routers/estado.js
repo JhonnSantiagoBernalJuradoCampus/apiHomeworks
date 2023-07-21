bd540fda8fa249f38dd1f5ba4c86ae745811c3e4
JhonnSantiagoBernalJuradoCampus <jhonbernalcampus@gmail.com>
2023-07-21 06:50:46 -0500

    feat: :sparkles: Get id
    Validacion de el estado

--- routers/estado.js
+++ routers/estado.js
@@ -1,15 +1,18 @@
 import { Router } from "express";
 import dotenv from "dotenv";
 import mysql from "mysql2";
+import proxyIds from "../middleware/proxyIds.js";
 dotenv.config();
 const appEstado = Router();
 
 let dbConfig = JSON.parse(process.env.DB_CONFIG);
 const con = mysql.createPool(dbConfig);
 
-appEstado.get('/', (req, res) => {
-    con.query(
-        /*sql*/`SELECT * FROM estado;`,
+appEstado.get('/:id?',proxyIds ,(req, res) => {
+    let sql = (req.params.id)
+    ? ['SELECT * FROM estado WHERE estado_id = ?', req.params.id]
+    : ['SELECT * FROM estado']
+    con.query(...sql,
         (err, data, fill)=>{
             res.send(data)
         }
