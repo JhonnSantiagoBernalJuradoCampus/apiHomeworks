2cc7a0bad4a5a72d82dcfb4be1591f02871369d2
JhonnSantiagoBernalJuradoCampus <jhonbernalcampus@gmail.com>
2023-07-26 08:25:37 -0500

    feat: :sparkles: Arreglo de la base de datos
    Union de los datos con la creacion

--- db/homeworks_db.sql
+++ db/homeworks_db.sql
@@ -33,3 +33,25 @@ CREATE TABLE tarea(
     FOREIGN KEY (id_estado) REFERENCES estado(estado_id),
     FOREIGN KEY (id_tipo) REFERENCES tipo(tipo_id)
 );
+INSERT INTO user (usu_nombre,usu_email,usu_telefono,usu_apodo, usu_created_at) VALUES 
+("Jhon Santiago Bernal Jurado", "jbernalsantiago11@gmail.com", "3123860656", "Bernal", CURDATE()),
+("Angie Nathalia Suarez Perez", "angienatalia@gmail.com", "3118128060", "Angie", CURDATE()),
+("Angela Esperanza Almeida Amaya", "angela@gmail.com", "3118124232", "Angela", CURDATE()),
+("James Ronald Bernal Bermudez", "ronald@gmail.com", "312741832", "James", CURDATE());
+
+INSERT INTO estado (estado_nombre) VALUES
+("Pendiente"),
+("Realizada");
+
+INSERT INTO tipo (tipo_nombre, tipo_created_at) VALUES
+("Ingles", CURDATE()),
+("Ser", CURDATE()),
+("Skills",CURDATE()),
+("Matematicas",CURDATE());
+
+INSERT INTO tarea (tarea_titulo, tarea_descripcion, tarea_fecha, tarea_recordatorio, tarea_created_at, id_user, id_tipo) VALUES
+('Backend', 'Realizar un proyecto con node', '2023-07-30', '2023-07-29', CURDATE(), 1, 3),
+('Figma', 'Realizar un figma de una pagina web', '2023-07-29', '2023-07-28', CURDATE(), 1, 2),
+('Presentacion', 'Hacer un video de presentacion personal en ingles', '2023-07-25', '2023-07-24', CURDATE(), 2, 1),
+('Frontend', 'Hacer una pagina de venta de zapatos con bootstrap 5', '2023-07-23', '2023-07-22', CURDATE(), 2, 3),
+('Ecuaciones', 'Resolver todo el taller de ecuaciones de la universidad', '2023-07-24', '2023-07-23', CURDATE(), 1, 4);
